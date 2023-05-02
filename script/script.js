bc0818899f673a476531386bdeee2c09c085b56b
Jonathan Koch <87237865+Jonathan-Koch@users.noreply.github.com>
2023-05-02 22:28:06 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Python Developer", "Blogger", "Fullstack Developer", "IT Specialist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
